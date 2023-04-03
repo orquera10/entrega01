3b796aee9465bc67dc6a6c80435a8dd4ea5e94b8
Dario <orquera10@gmail.com>
2023-04-03 01:09:18 -0300

    modificando entrga2

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
-const ProductManager = require("./manager/ProductManager.js");
 
-//Se creará una instancia de la clase “ProductManager”
-const productos = new ProductManager();
+// const ProductManager = require("./manager/ProductManager.js");
 
-//Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío [] 
-console.log(productos.getProducts());
+// //Se creará una instancia de la clase “ProductManager”
+// const productos = new ProductManager();
 
-//Se llamará al método “addProduct”
-productos.addProduct("producto prueba","Este es un producto de prueba",200,"sin imagen","abc123",25);
+// //Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío [] 
+// console.log(productos.getProducts());
 
-//El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-console.log(productos.getProducts());
+// //Se llamará al método “addProduct”
+// productos.addProduct("producto prueba","Este es un producto de prueba",200,"sin imagen","abc123",25);
 
-//Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
-productos.addProduct("producto prueba","Este es un producto de prueba",200,"sin imagen","abc123",25);
-console.log(productos.getProducts());
+// //El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+// console.log(productos.getProducts());
 
-//Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
-console.log(productos.getProductById(0));
+// //Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
+// productos.addProduct("producto prueba","Este es un producto de prueba",200,"sin imagen","abc123",25);
+// console.log(productos.getProducts());
+
+// //Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
+// console.log(productos.getProductById(0));
+
+
+
+
+// (title, description, price, thumbnail,code,stock)
+// const validarCode = this.products.find(item=>item.code===code);
+// if (validarCode) {
+//     console.log(`El code ${code} ya esta en uso`);
+//     return
+// }
+
+// if(!title || !description || !price || !thumbnail || !code || stock==null) {
+//     console.log(`Todos los campos son obligatorios.`);
+//     return
+// }
+
+
+// const product = {
+//     title,
+//     description,
+//     price,
+//     thumbnail,
+//     code,
+//     stock
+// };
+import ProductManager  from "./manager/ProductManager.js";
+
+const manager = new ProductManager(`./data/Products.json`)
+
+const env = async () => {
+    const productos = await manager.getProducts();
+    console.log(productos);
+
+    const producto = {
+        title: 'producto prueba',
+        description: 'Este es un producto de prueba',
+        price: 200,
+        thumbnail: 'sin imagen',
+        code: 'abc123',
+        stock: 25
+    };
+
+    await manager.addProduct(producto);
+
+    const productsResult = await manager.getProducts();
+    console.log(productsResult);
+
+    const productoMod = {
+        // title: 'producto pruebaC',
+        // description: 'Este es un producto de prueba',
+        price: 205,
+        // thumbnail: 'sin imagen',
+        code: 'abc124',
+        //stock: 29
+    };
+    await manager.updateProducts(10,productoMod);
+
+    await manager.deleteProduct(5);
+
+    console.log(await manager.getProductById(10));
+}
+
+// console.log(await products.getProducts());
+env()
--- manager/ProductManager.js
+++ manager/ProductManager.js
@@ -1,54 +1,104 @@
-class ProductManager {
+
+import fs from "fs";
+
+export default class ProductManager {
     constructor(path) {
         this.path = path;
     }
 
-    getProducts = () => {
-        return this.products;
+    getProducts = async () => {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, 'utf-8');
+                const products = JSON.parse(data);
+                return products;
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
 
-    addProduct = (title, description, price, thumbnail,code,stock) => {
+    addProduct = async (product) => {
+        try {
+            const products = await this.getProducts();
 
-        const product = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        };
+            if (products.length === 0) {
+                product.id = 1;
+            } else {
+                product.id = products[products.length - 1].id + 1;
+            }
 
-        const validarCode = this.products.find(item=>item.code===code);
+            products.push(product);
 
-        if (validarCode) {
-            console.log(`El code ${code} ya esta en uso`);
-            return
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+
+            return product;
+        } catch (error) {
+            console.log(error);
         }
-
-        if(!title || !description || !price || !thumbnail || !code || stock==null) {
-            console.log(`Todos los campos son obligatorios.`);
-            return
-        }
-
-        if (this.products.length === 0) {
-            product.id = 1;
-        } else {
-            product.id = this.products[this.products.length - 1].id + 1;
-        }
-        
-        this.products.push(product);
-        
     }
 
-    getProductById = (id) =>{
-        const productoEncontrado = this.products.find(item=>item.id===id)
-        if (productoEncontrado) {
-            return productoEncontrado;
-        } else {
-            return `Not found`;
+    getProductById = async (id) =>{
+        try {
+            const products = await this.getProducts();
+            const productoEncontrado = products.find(item=>item.id===id)
+            if (productoEncontrado) {
+                return productoEncontrado;
+            } else {
+                return `Not found`;
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    updateProducts = async (id, product) =>{
+        try {
+            const products = await this.getProducts();
+            const nuevoProducts = products.map(function (element) {
+                if (element.id === id) {
+                    return {...element,...product,id:id}   
+                } else {
+                    return element;
+                }
+            
+            });
+            await fs.promises.writeFile(this.path, JSON.stringify(nuevoProducts, null, '\t'));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // updateProducts = async (id, product) =>{
+    //     try {
+    //         const products = await this.getProducts();
+    //         let indice = products.findIndex( objeto => {
+    //             return objeto.id === id;
+    //         });
+    //         if (indice !== -1) {
+    //             products[indice] = { ...products[indice], ...product };
+    //         }
+
+    //         await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+
+    deleteProduct = async (id) =>{
+        try {
+            const products = await this.getProducts();
+            const nuevoProducts = products.filter( product => {
+                return product.id !== id;
+            });
+            await fs.promises.writeFile(this.path, JSON.stringify(nuevoProducts, null, '\t'));
+        } catch (error) {
+            console.log(error);
         }
     }
 }
-
-module.exports = ProductManager;
