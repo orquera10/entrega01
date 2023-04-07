d8c4cd8d5f4157076fdce82db2941ec310236694
Dario <orquera10@gmail.com>
2023-04-07 01:03:54 -0300

    entrega03

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import ProductManager from '../manager/ProductManager.js';
+
+const app = express();
+
+const productManager = new ProductManager('./data/Products.json');
+
+app.get('/products', async (req, res) => {
+    const productos = await productManager.getProducts();
+    const limite = Number(req.query.limite) || productos.length;
+    const productView = productos.slice(0,limite);
+    res.send(productView);
+});
+
+app.get('/products/:pid', async (req, res) => {
+    const productID = Number(req.params.pid);
+    const producto = await productManager.getProductById(productID);
+    res.send(producto);
+});
+
+app.listen(8080,()=>console.log("Listening on 8080"))
