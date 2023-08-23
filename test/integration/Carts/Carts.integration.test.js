481f908e9f1e17e54586a1a84759334f24029f5f
Dario <orquera10@gmail.com>
2023-08-22 22:04:17 -0300

    terminando entrega de testing

--- test/integration/Carts/Carts.integration.test.js
+++ test/integration/Carts/Carts.integration.test.js
@@ -25,69 +25,92 @@ describe('Testing de carts', () => {
         };
     });
 
-    it('POST de /api/products debe crear un producto correctamentte', async () => {
-        const productMock = {
-            title: "producto prueba dario",
-            description: "Este es un producto de prueba",
-            price: 200,
-            code: "abc13386889485",
-            category: "food",
-            stock: 25
-        };
-        
-        const { statusCode, _body } = await requester.post('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productMock);
-        expect(statusCode).to.be.eql(200);
-        expect(_body.data).to.have.property('_id');
+    it('POST de /api/carts debe crear un cart correctamentte y comprobar que tenga un array de products vacio', async () => {
+
+        const { statusCode, _body } = await requester.post('/api/carts').set('Cookie', [`${cookie.name}=${cookie.value}`]);
+            expect(statusCode).to.be.eql(200);
+            expect(_body.data).to.have.property('_id');
+            expect(Array.isArray(_body.data.products)).to.be.eql(true);
+            expect(_body.data.products).to.be.eql([]);
     });
 
-    it('POST de /api/products se debe corroborrar que el producto tenga el campo owner', async () => {
-        const productMock = {
-            title: "producto prueba dario",
-            description: "Este es un producto de prueba",
-            price: 200,
-            code: "abc13386885",
-            category: "food",
-            stock: 25
-        };
+    it('GET de /api/carts se debe corroborrar que se obtenga correctamente la información de un carrito', async () => {
 
-        const { statusCode, _body } = await requester.post('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productMock);
-
-        expect(statusCode).to.be.eql(200);
-        expect(_body.data).to.have.property('owner');
-    });
-
-    it('GET de /api/products se debe corroborrar que la respuesta tiene el campo data, además data debe ser un arreglo', async () => {
-        const { statusCode, _body } = await requester.get('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]);
-
-        expect(statusCode).to.be.eql(200);
-        expect(_body).to.have.property('data');
-        expect(Array.isArray(_body.data)).to.be.eql(true);
-    });
-
-    it('PUT de /api/products se debe corroborrar que se actualice correctamente la información del producto', async () => {
-        const productMock = {
-            title: "producto prueba dario",
-            description: "Este es un producto de prueba",
-            price: 200,
-            code: "abc133868896385",
-            category: "food",
-            stock: 25
-        };
-
-        const { _body } = await requester.post('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productMock);
+        const { _body } = await requester.post('/api/carts').set('Cookie', [`${cookie.name}=${cookie.value}`]);
 
         const id = _body.data._id;
 
-        const productUp = {
-            title: "producto prueba dario2",
-            description: "Este es un producto de prueba",
-            price: 200,
-            category: "food",
-            stock: 25
-        };
-
-        const putResult = await requester.put(`/api/products/${id}`).set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productUp);
+        const putResult = await requester.get(`/api/carts/${id}`).set('Cookie', [`${cookie.name}=${cookie.value}`]);
 
         expect(putResult.statusCode).to.be.eql(200);
     });
+
+
+
+
+    // it('POST de /api/products debe crear un producto correctamentte', async () => {
+    //     const productMock = {
+    //         title: "producto prueba dario",
+    //         description: "Este es un producto de prueba",
+    //         price: 200,
+    //         code: "abc13386889485",
+    //         category: "food",
+    //         stock: 25
+    //     };
+        
+    //     const { statusCode, _body } = await requester.post('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productMock);
+    //     expect(statusCode).to.be.eql(200);
+    //     expect(_body.data).to.have.property('_id');
+    // });
+
+    // it('POST de /api/products se debe corroborrar que el producto tenga el campo owner', async () => {
+    //     const productMock = {
+    //         title: "producto prueba dario",
+    //         description: "Este es un producto de prueba",
+    //         price: 200,
+    //         code: "abc13386885",
+    //         category: "food",
+    //         stock: 25
+    //     };
+
+    //     const { statusCode, _body } = await requester.post('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productMock);
+
+    //     expect(statusCode).to.be.eql(200);
+    //     expect(_body.data).to.have.property('owner');
+    // });
+
+    // it('GET de /api/products se debe corroborrar que la respuesta tiene el campo data, además data debe ser un arreglo', async () => {
+    //     const { statusCode, _body } = await requester.get('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]);
+
+    //     expect(statusCode).to.be.eql(200);
+    //     expect(_body).to.have.property('data');
+    //     expect(Array.isArray(_body.data)).to.be.eql(true);
+    // });
+
+    // it('PUT de /api/products se debe corroborrar que se actualice correctamente la información del producto', async () => {
+    //     const productMock = {
+    //         title: "producto prueba dario",
+    //         description: "Este es un producto de prueba",
+    //         price: 200,
+    //         code: "abc133868896385",
+    //         category: "food",
+    //         stock: 25
+    //     };
+
+    //     const { _body } = await requester.post('/api/products').set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productMock);
+
+    //     const id = _body.data._id;
+
+    //     const productUp = {
+    //         title: "producto prueba dario2",
+    //         description: "Este es un producto de prueba",
+    //         price: 200,
+    //         category: "food",
+    //         stock: 25
+    //     };
+
+    //     const putResult = await requester.put(`/api/products/${id}`).set('Cookie', [`${cookie.name}=${cookie.value}`]).send(productUp);
+
+    //     expect(putResult.statusCode).to.be.eql(200);
+    // });
 });
