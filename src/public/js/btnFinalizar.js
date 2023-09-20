dc96778a5ded28a29261336e485c2cdf335dd7e8
Dario <orquera10@gmail.com>
2023-09-20 19:06:57 -0300

    entrega final

--- src/public/js/btnFinalizar.js
+++ src/public/js/btnFinalizar.js
@@ -25,7 +25,7 @@ btnDel.addEventListener('click', function () {
 })
 
 btnFin.addEventListener(`click`, function () {
-    
+
 
     fetch(`/api/carts/${cid}/purchase`, {
         method: 'POST',
@@ -43,15 +43,60 @@ btnFin.addEventListener(`click`, function () {
             const datos = data.data;
             Swal.fire({
                 icon: 'success',
-                title: "¡Ticket generado con éxito!",
-                text: `Ticket id: ${datos.ticket._id} generado exitosamente`,
+                html: `<!DOCTYPE html>
+                        <html lang="es">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Detalles del Ticket</title>
+                            <style>
+                                table {
+                                    border-collapse: collapse;
+                                    width: 50%;
+                                }
+
+                                th, td {
+                                    border: 1px solid black;
+                                    padding: 8px;
+                                    text-align: left;
+                                }
+                            </style>
+                        </head>
+                        <body>
+
+                        <h1>Detalles del Ticket</h1>
+
+                        <table>
+                            <tr>
+                                <th>ID del Ticket</th>
+                                <td id="ticketId">${datos.ticket._id}</td>
+                            </tr>
+                            <tr>
+                                <th>Código</th>
+                                <td id="ticketCode">${datos.ticket.code}</td>
+                            </tr>
+                            <tr>
+                                <th>Cantidad</th>
+                                <td id="ticketAmount">${datos.ticket.amount}</td>
+                            </tr>
+                            <tr>
+                                <th>Comprador</th>
+                                <td id="ticketPurchaser">${datos.purchaser}</td>
+                            </tr>
+                            <tr>
+                                <th>Fecha de Compra</th>
+                                <td id="ticketPurchaseDatetime">${datos.ticket.purchase_datetime}</td>
+                            </tr>
+                        </table>
+                        </body>
+                        </html>
+                        `
+                // title: "¡Ticket generado con éxito!",
+                // text: `Ticket id: ${datos.ticket._id} generado exitosamente`,
             }).then((result) => {
                 if (result.isConfirmed) {
                     window.location.href = '/cart';
                 }
             });
-            console.log(datos);
-            // setTimeout(() => { window.location.replace(`/cart`); }, 2500)
         })
         .catch(error => {
             console.error('Error al realizar la solicitud POST:', error);
